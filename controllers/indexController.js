295087113d3f1b36623789c6855710966b7eea49
lorenzo774 <lorenzoconti774@gmail.com>
2022-04-29 12:04:40 +0200

    Fixed redirect index

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,26 +3,30 @@ const Product = require("../models/product");
 const async = require("async");
 
 module.exports.index = function (req, res, next) {
-  async.parallel(
-    {
-      users_count: function (callback) {
-        User.countDocuments().exec(callback);
+  if (req.user) {
+    res.redirect("/account");
+  } else {
+    async.parallel(
+      {
+        users_count: function (callback) {
+          User.countDocuments().exec(callback);
+        },
+        products_count: function (callback) {
+          Product.countDocuments().exec(callback);
+        },
       },
-      products_count: function (callback) {
-        Product.countDocuments().exec(callback);
-      },
-    },
-    function (err, results) {
-      if (err) {
-        return next(err);
+      function (err, results) {
+        if (err) {
+          return next(err);
+        }
+        // If there are no errors
+        const { users_count, products_count } = results;
+        res.render("index", {
+          title: "Welcome to lorenzo-e-commerce",
+          users: users_count,
+          products: products_count,
+        });
       }
-      // If there are no errors
-      const { users_count, products_count } = results;
-      res.render("index", {
-        title: "Welcome to lorenzo-e-commerce",
-        users: users_count,
-        products: products_count,
-      });
-    }
-  );
+    );
+  }
 };
